1ce8bfec29f38762482eb370e6d06a5cec96f942
Matheus Araújo <souzamatheus8174@gmail.com>
2025-04-26 17:25:33 -0300

    feat: create app main layout

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -2,10 +2,13 @@ import '@/global.css';
 
 import 'react-native-reanimated';
 
+import { View } from 'react-native';
 import { Stack } from 'expo-router';
 import { ThemeProvider } from '@react-navigation/native';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 import { useThemeConfig } from '@/lib/use-theme-config';
+import colors from '@/components/ui/colors';
 
 export const unstable_settings = {
   initialRouteName: '(tabs)'
@@ -17,11 +20,26 @@ function RootLayout() {
   const theme = useThemeConfig();
 
   return (
-    <ThemeProvider value={theme}>
-      <Stack>
-        <Stack.Screen />
-      </Stack>
-    </ThemeProvider>
+    <View className="flex-1 bg-black-600">
+      <KeyboardProvider>
+        <ThemeProvider value={theme}>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              statusBarStyle: 'light',
+              statusBarBackgroundColor: colors.black[600],
+              navigationBarColor: colors.black[600]
+            }}
+          >
+            <Stack.Screen name="index" redirect />
+
+            <Stack.Screen name="(register)/personal-data" />
+
+            {/* <Stack.Screen name="register/index" redirect /> */}
+          </Stack>
+        </ThemeProvider>
+      </KeyboardProvider>
+    </View>
   );
 }
 
