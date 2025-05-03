71c57467bb30005af27299d550febd7462c6aa32
Matheus Araújo <souzamatheus8174@gmail.com>
2025-05-03 10:04:12 -0300

    refactor: update user schema

--- mobile/src/schemas/createUserSchema.ts
+++ mobile/src/schemas/createUserSchema.ts
@@ -1,15 +1,98 @@
 import { z } from 'zod';
 
-export const contentPreferencesSchema = z.object({
-  commonPreferences: z.array(z.string()),
-  preferencesByGame: z.object({
-    name: z.string(),
-    preferences: z.array(z.string())
-  })
+export const games = [
+  'CS2 (Counter-Strike 2)',
+  'Valorant',
+  'PUBG',
+  'League of Legends',
+  'Fortnite',
+  'Dota 2',
+  'Rainbow Six Siege',
+  'Call of Duty',
+  'Apex Legends',
+  'Rocket League',
+  'Free Fire'
+] as const;
+
+export const platforms = [
+  'Twitch',
+  'YouTube',
+  'TikTok',
+  'Instagram',
+  'Twitter (X)',
+  'Discord',
+  'Facebook Gaming',
+  'Trovo',
+  'Kick'
+] as const;
+
+export const orgs = [
+  'FURIA',
+  'LOUD',
+  'Fluxo',
+  'MIBR',
+  'Pain Gaming',
+  'RED Canids',
+  'Team Liquid',
+  'Sentinels',
+  'G2 Esports',
+  'FaZe Clan',
+  'Cloud9',
+  'Ninjas in Pyjamas'
+] as const;
+
+export const events = [
+  'IEM Rio Major',
+  'CBLOL/LTASUL',
+  'Valorant Champions Tour',
+  'Game XP',
+  'TwitchCon',
+  'BLAST Premier',
+  'DreamHack',
+  'Comic Con Experience (CCXP)',
+  'BGS (Brasil Game Show)'
+] as const;
+
+export const products = [
+  'Ingressos para campeonatos',
+  'Camisas ou produtos oficiais',
+  'Equipamentos gamer (mouse, teclado, headset)',
+  'Serviços de assinatura (pass de batalha, clubes, etc.)',
+  'Nenhuma compra'
+] as const;
+
+export const gender = [
+  'Masculino',
+  'Feminino',
+  'Não-binário',
+  'Prefiro não informar',
+  'Outro'
+] as const;
+
+const createInterestsFactory = (
+  options: readonly [string, ...string[]],
+  errorMessage: string
+) =>
+  z
+    .array(
+      z.object({
+        name: z.enum(options),
+        selected: z.boolean()
+      })
+    )
+    .refine((data) => data.find(({ selected }) => selected), errorMessage);
+
+export const interestsActivitiesSchema = z.object({
+  spentMoney: z.coerce.number().min(0.0),
+  orgs: createInterestsFactory(orgs, 'Escolha ao menos uma organização'),
+  games: createInterestsFactory(games, 'Escolha ao menos um jogo competitivo'),
+  events: createInterestsFactory(events, 'Escolha ao menos um evento'),
+  products: createInterestsFactory(products, 'Escolha ao menos uma opção'),
+  platforms: createInterestsFactory(platforms, 'Escolha ao menos uma opção')
 });
 
 export const createUserSchema = z.object({
-  name: z
+  nick: z
     .string({ required_error: 'Nome é obrigatório' })
     .min(4, 'Nome precisa ter no mínimo 4 caracteres'),
   email: z
@@ -21,13 +104,16 @@ export const createUserSchema = z.object({
 });
 
 export const personalDataSchema = z.object({
+  gender: z.enum(gender),
   phoneNumber: z
-    .string()
+    .string({ required_error: 'Informe o seu número de telefone' })
     .min(15, 'Número de telefone inválido')
     .max(16, 'Número de telefone inválido'),
-  name: z.string(),
+  name: z
+    .string({ required_error: 'Informe o seu nome' })
+    .min(2, 'Nome muito pequeno'),
   cpf: z
-    .string({ required_error: 'Informe o sue cpf' })
+    .string({ required_error: 'Informe o seu cpf' })
     .length(14, 'cpf muito pequeno'),
   birthdate: z
     .string({
@@ -44,18 +130,27 @@ export const personalDataSchema = z.object({
 
 export const locationSchema = z.object({
   cep: z.string({ required_error: 'Digite seu cep' }).length(9, 'Cep inválido'),
-  state: z.string({ required_error: 'Informe o seu Estado' }).min(2),
   city: z.string({ required_error: 'Informe a sua cidade' }).min(2),
-  neighborhood: z.string({ required_error: 'Informe o seu bairro' }),
+  state: z.string({ required_error: 'Informe o seu Estado' }).min(2),
   number: z.string({ required_error: 'Informe o número' }),
   adreass: z.string({ required_error: 'Informe o seu endereço' }).min(2),
-  complement: z.string().optional()
+  complement: z.string().optional(),
+  neighborhood: z.string({ required_error: 'Informe o seu bairro' })
 });
 
-export type ContentPreferencesSchema = z.infer<typeof contentPreferencesSchema>;
+export const validateDataSchema = z.object({
+  rgPhoto: z.array(z.string()),
+  facePhoto: z.string()
+});
 
 export type PersonalDataSchema = z.infer<typeof personalDataSchema>;
 
 export type LocationSchema = z.infer<typeof locationSchema>;
 
 export type CreateUserSchema = z.infer<typeof createUserSchema>;
+
+export type InterestsActivitiesSchema = z.infer<
+  typeof interestsActivitiesSchema
+>;
+
+export type ValidateDataSchema = z.infer<typeof validateDataSchema>;
