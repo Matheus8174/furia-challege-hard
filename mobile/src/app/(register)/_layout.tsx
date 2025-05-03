9c7ccc3f64005e8fb73c2e79968f66eebaa8cea9
Matheus Araújo <souzamatheus8174@gmail.com>
2025-05-03 09:22:07 -0300

    refactor: update global form

--- mobile/src/app/(register)/_layout.tsx
+++ mobile/src/app/(register)/_layout.tsx
@@ -1,30 +1,78 @@
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import {
-  contentPreferencesSchema,
   createUserSchema,
+  events,
+  products,
+  games,
+  interestsActivitiesSchema,
   locationSchema,
-  personalDataSchema
+  orgs,
+  personalDataSchema,
+  platforms,
+  validateDataSchema
 } from '@/schemas/createUserSchema';
+import colors from '@/components/ui/colors';
 
-const fullSchema = contentPreferencesSchema
-  .merge(createUserSchema)
+const fullSchema = createUserSchema
   .merge(personalDataSchema)
-  .merge(locationSchema);
+  .merge(locationSchema)
+  .merge(interestsActivitiesSchema)
+  .merge(validateDataSchema);
+
+export type FullSchema = Zod.infer<typeof fullSchema>;
+
+const createInterestsFactory = (interest: readonly [...string[]]) =>
+  interest.map((name) => ({ name, selected: false }));
+
+const values = {
+  orgs: createInterestsFactory(orgs),
+  games: createInterestsFactory(games),
+  events: createInterestsFactory(events),
+  products: createInterestsFactory(products),
+  platforms: createInterestsFactory(platforms)
+};
 
 function Layout() {
   const methods = useForm({
     resolver: zodResolver(fullSchema),
-    mode: 'all'
+    defaultValues: {
+      ...values,
+      spentMoney: 0,
+      rgPhoto: [],
+      gender: 'Prefiro não informar'
+    }
   });
 
   return (
     <FormProvider {...methods}>
-      <Slot />
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="index" />
+
+        <Stack.Screen name="interests" />
+
+        <Stack.Screen name="location-data" />
+
+        <Stack.Screen name="documents-validation" />
+
+        <Stack.Screen
+          name="choose-chip"
+          options={{
+            navigationBarColor: colors.black[200],
+            gestureEnabled: false,
+            animation: 'slide_from_bottom',
+            presentation: 'transparentModal'
+          }}
+        />
+      </Stack>
     </FormProvider>
   );
 }
