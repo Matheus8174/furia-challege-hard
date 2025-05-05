9ce2774f9663e1c25dab4b67f8b2a2cc047b358f
Matheus Araújo <souzamatheus8174@gmail.com>
2025-05-04 21:09:20 -0300

    feat: create schema constant

---
+++ server/src/modules/accounts/schemas/constants.ts
@@ -0,0 +1,68 @@
+export const gender = [
+  'Masculino',
+  'Feminino',
+  'Não-binário',
+  'Prefiro não informar',
+  'Outro'
+] as const;
+
+export const products = [
+  'Ingressos para campeonatos',
+  'Camisas ou produtos oficiais',
+  'Equipamentos gamer (mouse, teclado, headset)',
+  'Serviços de assinatura (pass de batalha, clubes, etc.)',
+  'Nenhuma compra'
+] as const;
+
+export const games = [
+  'CS2 (Counter-Strike 2)',
+  'Valorant',
+  'PUBG',
+  'League of Legends',
+  'Fortnite',
+  'Dota 2',
+  'Rainbow Six Siege',
+  'Call of Duty',
+  'Apex Legends',
+  'Rocket League',
+  'Free Fire'
+] as const;
+
+export const platforms = [
+  'Twitch',
+  'YouTube',
+  'TikTok',
+  'Instagram',
+  'Twitter (X)',
+  'Discord',
+  'Facebook Gaming',
+  'Trovo',
+  'Kick'
+] as const;
+
+export const orgs = [
+  'FURIA',
+  'LOUD',
+  'Fluxo',
+  'MIBR',
+  'Pain Gaming',
+  'RED Canids',
+  'Team Liquid',
+  'Sentinels',
+  'G2 Esports',
+  'FaZe Clan',
+  'Cloud9',
+  'Ninjas in Pyjamas'
+] as const;
+
+export const events = [
+  'IEM Rio Major',
+  'CBLOL/LTASUL',
+  'Valorant Champions Tour',
+  'Game XP',
+  'TwitchCon',
+  'BLAST Premier',
+  'DreamHack',
+  'Comic Con Experience (CCXP)',
+  'BGS (Brasil Game Show)'
+] as const;
